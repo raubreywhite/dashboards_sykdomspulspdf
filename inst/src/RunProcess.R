f1790db1550cdb1e78cd16813998f3e784da3ca8
Travis CI User <travis@example.org>
2018-11-07 11:50:50 +0000

    Travis CI update 11 [ci skip]

--- inst/src/RunProcess.R
+++ inst/src/RunProcess.R
@@ -1,14 +1,15 @@
-#devtools::load_all("fhi")
+# devtools::load_all("fhi")
 fhi::DashboardInitialiseOpinionated(
-  NAME="sykdomspulspdf",
-  PACKAGE_DIR=".")
+  NAME = "sykdomspulspdf",
+  PACKAGE_DIR = "."
+)
 
 suppressMessages(library(data.table))
 suppressMessages(library(ggplot2))
-#devtools::use_package("odfWeave")
+# devtools::use_package("odfWeave")
 
 
-files <- list.files(fhi::DashboardFolder("data_raw"),"^partially_formatted_")
+files <- list.files(fhi::DashboardFolder("data_raw"), "^partially_formatted_")
 mydate <- format(Sys.time(), "%d.%m.%y")
 
 # fhi::DashboardIsDev()
@@ -18,72 +19,67 @@ list.files("/data_raw")
 fhi::DashboardMsg("/data_raw/sykdomspuls_pdf")
 list.files("/data_raw/sykdomspuls_pdf")
 
-if(length(files)==0){
+if (length(files) == 0) {
   fhi::DashboardMsg("No data")
-  quit(save="no", status=0)
-
+  quit(save = "no", status = 0)
 } else {
-  for(f in files) fhi::DashboardMsg(f)
+  for (f in files) fhi::DashboardMsg(f)
   # grab the latest
   useFile <- max(files)
 
-  if(file.exists(fhi::DashboardFolder("results","DONE.txt")) & !fhi::DashboardIsDev()){
+  if (file.exists(fhi::DashboardFolder("results", "DONE.txt")) & !fhi::DashboardIsDev()) {
     fhi::DashboardMsg("results DONE.txt exists")
-    quit(save="no", status=0)
+    quit(save = "no", status = 0)
   }
 
-  d <- fread(fhi::DashboardFolder("data_raw",useFile))
-  fylke <-fread(system.file("extdata", "fylke.csv", package = "sykdomspulspdf"))
-  lastestUpdate <- as.Date(gsub("_","-",LatestRawID()))
+  d <- fread(fhi::DashboardFolder("data_raw", useFile))
+  fylke <- fread(system.file("extdata", "fylke.csv", package = "sykdomspulspdf"))
+  lastestUpdate <- as.Date(gsub("_", "-", LatestRawID()))
 
   fhi::DashboardMsg("Generating monthly pdf")
 
-  #Alle konsultasjoner:
+  # Alle konsultasjoner:
   data <- CleanData(d)
-  alle <- tapply(data$gastro, data[, c("year","week")], sum)
-  weeknow <-findLastWeek(lastestUpdate,alle) ### need to be fixed
-  cat(paste("Last week",weeknow,sep = " "))
+  alle <- tapply(data$gastro, data[, c("year", "week")], sum)
+  weeknow <- findLastWeek(lastestUpdate, alle) ### need to be fixed
+  cat(paste("Last week", weeknow, sep = " "))
 
-  ##BY FYLKE
+  ## BY FYLKE
   for (SYNDROM in CONFIG$SYNDROMES) {
-    sykdompulspdf_template_copy(fhi::DashboardFolder("data_raw"),SYNDROM)
+    sykdompulspdf_template_copy(fhi::DashboardFolder("data_raw"), SYNDROM)
     fhi::sykdompulspdf_resources_copy(fhi::DashboardFolder("data_raw"))
 
-    if (SYNDROM=="mage") {
-      add="magetarm"
-      mytittle="Mage-tarminfeksjoner"
-
-    } else if (SYNDROM=="luft") {
-      add="luftvei"
-      mytittle="Luftveisinfeksjoner"
-
+    if (SYNDROM == "mage") {
+      add <- "magetarm"
+      mytittle <- "Mage-tarminfeksjoner"
+    } else if (SYNDROM == "luft") {
+      add <- "luftvei"
+      mytittle <- "Luftveisinfeksjoner"
     }
 
     ###########################################
     for (f in fylke$Fylkename) {
       fhi::DashboardMsg(sprintf("PDF: %s", f))
 
-      Fylkename=f
-      data <- CleanDataByFylke(d, fylke,f)
-      alle <- tapply(getdataout(data,SYNDROM), data[, c("year","week")], sum)
-      yrange <- max(alle,na.rm=T)+(roundUpNice(max(alle,na.rm=T))*.20)
+      Fylkename <- f
+      data <- CleanDataByFylke(d, fylke, f)
+      alle <- tapply(getdataout(data, SYNDROM), data[, c("year", "week")], sum)
+      yrange <- max(alle, na.rm = T) + (roundUpNice(max(alle, na.rm = T)) * .20)
 
 
-      #fhi::RenderExternally()
-      rmarkdown::render(input = fhi::DashboardFolder("data_raw",paste("monthly_report_",SYNDROM,".Rmd",sep="")),
-                        output_file = paste(gsub(" ", "", f, fixed = TRUE),"_",add,".pdf", sep=""),
-                        output_dir = fhi::DashboardFolder("results",paste("PDF",mydate,sep="_")))
-
+      # fhi::RenderExternally()
+      rmarkdown::render(
+        input = fhi::DashboardFolder("data_raw", paste("monthly_report_", SYNDROM, ".Rmd", sep = "")),
+        output_file = paste(gsub(" ", "", f, fixed = TRUE), "_", add, ".pdf", sep = ""),
+        output_dir = fhi::DashboardFolder("results", paste("PDF", mydate, sep = "_"))
+      )
     }
 
-    sykdompulspdf_template_remove(fhi::DashboardFolder("data_raw"),SYNDROM)
-
+    sykdompulspdf_template_remove(fhi::DashboardFolder("data_raw"), SYNDROM)
   }
 
   fhi::sykdompulspdf_resources_remove(fhi::DashboardFolder("data_raw"))
-
 }
 
-file.create(fhi::DashboardFolder("results","DONE.txt"))
-if(!fhi::DashboardIsDev()) quit(save="no", status=0)
-
+file.create(fhi::DashboardFolder("results", "DONE.txt"))
+if (!fhi::DashboardIsDev()) quit(save = "no", status = 0)
--- inst/src/RunTest.R
+++ inst/src/RunTest.R
@@ -1,42 +1,39 @@
-con <- file("/tmp/computer","r")
-COMPUTER_NAME <- readLines(con,n=1)
+con <- file("/tmp/computer", "r")
+COMPUTER_NAME <- readLines(con, n = 1)
 close(con)
-Sys.setenv(COMPUTER=COMPUTER_NAME)
+Sys.setenv(COMPUTER = COMPUTER_NAME)
 
-for(baseFolder in c("/data_clean","/results","/data_app")){
-  files <- list.files(file.path(baseFolder,"sykdomspulspdf"))
-  if(length(files)>0){
-    for(f in files) unlink(file.path(baseFolder,"sykdomspulspdf",f))
+for (baseFolder in c("/data_clean", "/results", "/data_app")) {
+  files <- list.files(file.path(baseFolder, "sykdomspulspdf"))
+  if (length(files) > 0) {
+    for (f in files) unlink(file.path(baseFolder, "sykdomspulspdf", f))
   }
 }
 
-unlink(file.path("/junit","sykdomspulspdf.xml"))
+unlink(file.path("/junit", "sykdomspulspdf.xml"))
 Sys.sleep(1)
 
 a <- testthat:::JunitReporter$new()
 a$start_reporter()
-a$out <- file(file.path("/junit","sykdomspulspdf.xml"), "w+")
+a$out <- file(file.path("/junit", "sykdomspulspdf.xml"), "w+")
 a$start_context("sykdomspulspdf")
 
-#if(FALSE){
-  output <- processx::run("Rscript","/r/sykdomspulspdf/src/RunProcess.R", error_on_status=F, echo=T)
-  cat("\n\nstdout\n\n")
-  cat(output$stdout)
-  cat("\n\nstderr\n\n")
-  cat(output$stderr)
+# if(FALSE){
+output <- processx::run("Rscript", "/r/sykdomspulspdf/src/RunProcess.R", error_on_status = F, echo = T)
+cat("\n\nstdout\n\n")
+cat(output$stdout)
+cat("\n\nstderr\n\n")
+cat(output$stderr)
 
-  if(output$status==0){
-    a$add_result("sykdomspulspdf","RunAll",testthat::expectation("success","Pass"))
-  } else {
-    a$add_result("sykdomspulspdf","RunAll",testthat::expectation("error","Fail"))
-  }
-#} else {
-  a$add_result("sykdomspulspdf","RunAll",testthat::expectation("success","Pass"))
-#}
+if (output$status == 0) {
+  a$add_result("sykdomspulspdf", "RunAll", testthat::expectation("success", "Pass"))
+} else {
+  a$add_result("sykdomspulspdf", "RunAll", testthat::expectation("error", "Fail"))
+}
+# } else {
+a$add_result("sykdomspulspdf", "RunAll", testthat::expectation("success", "Pass"))
+# }
 
 a$end_context("sykdomspulspdf")
 a$end_reporter()
 close(a$out)
-
-
-
