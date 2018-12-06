8d27e1670a1ec96648d6105cbd746da1bf400501
Travis CI User <travis@example.org>
2018-12-06 14:00:41 +0000

    Travis CI update 15 [ci skip]

--- inst/src/RunProcess.R
+++ inst/src/RunProcess.R
@@ -1,8 +1,9 @@
 # devtools::load_all("fhi")
 fhi::DashboardInitialiseOpinionated(
-  NAME="sykdomspulspdf",
-  PKG="sykdomspulspdf",
-  PACKAGE_DIR=".")
+  NAME = "sykdomspulspdf",
+  PKG = "sykdomspulspdf",
+  PACKAGE_DIR = "."
+)
 
 suppressMessages(library(data.table))
 suppressMessages(library(ggplot2))
@@ -31,16 +32,16 @@ if (length(files) == 0) {
     quit(save = "no", status = 0)
   }
 
-  if(RAWmisc::IsFileStable(fhi::DashboardFolder("data_raw",useFile))==F) {
+  if (RAWmisc::IsFileStable(fhi::DashboardFolder("data_raw", useFile)) == F) {
     fhi::DashboardMsg("file no stable")
-    quit(save="no", status=0)
+    quit(save = "no", status = 0)
   }
 
 
 
-  d <- fread(fhi::DashboardFolder("data_raw",useFile))
-  fylke <-fread(system.file("extdata", "fylke.csv", package = "sykdomspulspdf"))
-  lastestUpdate <- as.Date(gsub("_","-",LatestRawID()))
+  d <- fread(fhi::DashboardFolder("data_raw", useFile))
+  fylke <- fread(system.file("extdata", "fylke.csv", package = "sykdomspulspdf"))
+  lastestUpdate <- as.Date(gsub("_", "-", LatestRawID()))
 
 
   fhi::DashboardMsg("Generating monthly pdf")
